a01cf8623c0c1eda8483a3d06541980861cc9804
afdaaaledu@gmail.com <afdaaaledu@gmail.com>
2026-04-08 21:19:51 +0530

    4th

---
+++ hogwards.py
@@ -0,0 +1,16 @@
+students = [
+{"name": "Harry", "House": "Grif", "Patron":"Horse"},
+{"name":"Ron", "House" : "Grif", "Patron": "Stag"}
+]
+
+for student in students:
+    print (student["name"], student ["House"], student["Patron"], sep=", ")
+    
+
+
+
+
+
+
+
+
